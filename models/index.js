bbce38a07f7501b167ec014330bec81b5c95556f
JeongMin Lee <jmboc.0501@gmail.com>
2022-03-06 18:18:04 +0900

    postgres sequelize 연동

--- models/index.js
+++ models/index.js
@@ -1,31 +1,59 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+console.log(config);
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+const DATA_BASE_URL =
+  "postgres://yayhfoizruvvhd:608887976c3155d72e87feea8fbc178f4783e503f5b3e6e801174dd948764e06@ec2-35-153-35-94.compute-1.amazonaws.com:5432/dbc5r4cjb6ejvf";
+
+let sequelize;
+sequelize = new Sequelize(DATA_BASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+sequelize
+  .authenticate()
+  .then(() => {})
+  .catch((err) => {});
+
+// if (DATA_BASE_URL) {
+//   sequelize = new Sequelize(DATA_BASE_URL, {
+//     dialect: "postgres",
+//     native: true,
+//     protocol: "postgres",
+//   });
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js";
   })
-  .forEach(file => {
+  .forEach((file) => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
@@ -35,3 +63,5 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
+// postgres://yayhfoizruvvhd:608887976c3155d72e87feea8fbc178f4783e503f5b3e6e801174dd948764e06@ec2-35-153-35-94.compute-1.amazonaws.com:5432/dbc5r4cjb6ejvf
